b2c9231ca6961586b99cf1e7715af3ba7de4a4a6
Katie Lewis <klewi314@gmail.com>
2019-05-18 10:27:35 -0700

    Added schema and server

--- app/schema.sql
+++ app/schema.sql
@@ -3,4 +3,11 @@ USE todo;
 
 CREATE TABLE 'todo_items'(
   
+`id
+` Int
+( 11 ) AUTO_INCREMENT NOT NULL,
+isDone boolean
+not null default 0
+PRIMARY KEY
+(`id`)
 )
---
+++ app/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes")(app);
+
+require("./config")
+  .sync()
+  .then(_ => app.listen(3000))
+  .catch(e => console.log(e));
